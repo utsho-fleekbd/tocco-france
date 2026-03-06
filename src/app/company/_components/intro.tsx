35ecbd6984d7ed2899632ae669c5422122ed053a
Utsho MH <utshomh.dev@gmail.com>
2026-03-06 22:48:57 +0600

    added animation to career page

--- src/app/company/_components/intro.tsx
+++ src/app/company/_components/intro.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 import eiffelTower from "../assets/eiffel-tower.jpg";
 
@@ -16,13 +19,23 @@ export function Intro() {
       <div className="absolute inset-0 bg-[#0d1d42]/75"></div>
       <div className="absolute inset-0 bg-linear-to-b from-black/25 via-transparent to-black/25"></div>
 
-      <p className="relative z-10 max-w-4/6 text-center text-sm md:text-lg font-normal md:font-semibold">
+      <motion.p
+        className="relative z-10 max-w-4/6 text-center text-sm md:text-xl"
+        initial={{ y: 75, opacity: 0 }}
+        whileInView={{
+          y: 0,
+          opacity: 1,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+      >
         With a perfect blend of authenticity and quality Tocco France delighted
         to offer engraving service across full range of luxury switches and
         sockets. Beautifully designed and meticulously engineered, these luxury
         sockets and switches can truly reflect your style and design flair at
         your home.
-      </p>
+      </motion.p>
     </div>
   );
 }
--- src/app/company/_components/vision.tsx
+++ src/app/company/_components/vision.tsx
@@ -1,10 +1,30 @@
+"use client";
+
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 import ourVision from "../assets/our-vision.png";
 
 export function Vision() {
   return (
-    <div className="p-16 bg-background-2 flex flex-col items-center justify-center gap-8 rounded-2xl text-center">
+    <motion.div
+      className="p-16 bg-background-2 flex flex-col items-center justify-center gap-8 rounded-2xl text-center"
+      initial={{
+        rotateY: 40,
+        y: 30,
+        x: -15,
+        scale: 0.8,
+      }}
+      whileInView={{
+        rotateY: 0,
+        y: 0,
+        x: 0,
+        scale: 1,
+      }}
+      transition={{
+        duration: 0.7,
+      }}
+    >
       <Image
         src={ourVision}
         alt="Mission"
@@ -13,20 +33,40 @@ export function Vision() {
         className="size-42 object-cover"
       />
 
-      <h2 className="uppercase font-semibold tracking-widest text-3xl">
+      <motion.h2
+        className="uppercase font-semibold tracking-widest text-3xl"
+        initial={{ y: -25, opacity: 0 }}
+        whileInView={{
+          y: 0,
+          opacity: 1,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+      >
         Our Vision
-      </h2>
+      </motion.h2>
 
       <div className="h-1.5 w-32 bg-white"></div>
 
-      <p className="text-sm md:text-base">
+      <motion.p
+        className="text-sm md:text-base"
+        initial={{ y: 35, opacity: 0 }}
+        whileInView={{
+          y: 0,
+          opacity: 1,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+      >
         Making your dream home is the first step to living your greatest life.
         To suit your preferences, we provide a great selection of switches and
         sockets in both vintage and fascinating styles. By offering premium
         consumers with design, quality, and service, we establish Tocco France
         as the most valuable brand in the world. We do this by designing sockets
         and switches with your needs in mind.
-      </p>
-    </div>
+      </motion.p>
+    </motion.div>
   );
 }
